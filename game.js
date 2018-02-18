0bef824038e9781d9c9ee307ca28a0c9f47a5cf9
FarrukhKhalikoff <farrukhkhalikov@yahoo.com>
2018-02-18 14:49:26 -0500

    added first .js

---
+++ game.js
@@ -0,0 +1,13 @@
+var question = confirm("Are you sure you want to send a payment?");
+
+if (question == true) {
+
+    x = "Payment sent!";
+
+} else {
+
+    x = "Payment cancelled!";
+
+}
+
+alert (x);
