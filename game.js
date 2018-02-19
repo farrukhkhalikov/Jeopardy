f6830ce6ec8ef6b4a52b0a363e66308437330d8e
FarrukhKhalikoff <farrukhkhalikov@yahoo.com>
2018-02-19 11:13:57 -0500

    working on scoreboard logic

--- game.js
+++ game.js
@@ -1,34 +1,142 @@
 // Puts score onto page in scoreboard div
 let score = 0;
+let answers = ''
 $('.scoreboard').html(score)
 
 // displays question
-$('.his-100').on('click', () => {
-    $('.questions').html('Christopher Columbus sailed the ocean blue on this year');
-    $('.answerA').html('when is 1492?');
-    $('.answerB').html('when is 1266?');
-    $('.answerC').html('when is 1345?');
-    $(event.target).off();
+$('.his-100').one('click', () => {
+    $('.questions').html('Ulysses S. Grant appears on the front of which denomination of US. currency');
+    $('.answerA').html('$50 bill');
+    $('.answerB').html('$20 bill');
+    $('.answerC').html('$5 bill');
+    //$(event.target).off();
     $(event.target).css( "color", "red");
-})
-
-
-// correct or incorrect answers
+    answers = "A"
+    let playerAnswer = ""
+    // correct or incorrect answers
     $('#button-A').on('click', () => {
-        $('.comment').html('That is the correct answer');
-        $('.scoreboard').html(score += 100)
+        playerAnswer = "A"
+        if(playerAnswer === answers){
+            $('.scoreboard').html(score += 100)
+            $('.comment').html('That is the correct answer');        
+        } else {
+            $('.scoreboard').html(score -= 100)
+            $('.comment').html('That is the incorrect answer');       
+        }     
         $(event.target).off()
         })
 
     $('#button-B').on('click', () => {
-        $('.comment').html('That is the incorrect answer');
-        $('.scoreboard').html(score -= 100)
+        playerAnswer = "B"
+        if(playerAnswer === answers){
+            $('.scoreboard').html(score += 100)
+            $('.comment').html('That is the correct answer');        
+        } else {
+            $('.scoreboard').html(score -= 100)
+            $('.comment').html('That is the incorrect answer');       
+        }     
         $(event.target).off()
     })
 
     $('#button-C').on('click', () => {
-        $('.comment').html('That is the incorrect answer');
-        $('.scoreboard').html(score -= 100)
+        playerAnswer = "C"
+        if(playerAnswer === answers){
+            $('.scoreboard').html(score += 100)
+            $('.comment').html('That is the correct answer');        
+        } else {
+            $('.scoreboard').html(score -= 100)
+            $('.comment').html('That is the incorrect answer');       
+        }     
         $(event.target).off()
     });
+    
+})
 
+
+// correct or incorrect answers
+    // $('#button-A').on('click', () => {
+    //     $('.comment').html('That is the correct answer');
+    //     $('.scoreboard').html(score += 100)
+    //     $(event.target).off()
+    //     })
+
+    // $('#button-B').on('click', () => {
+    //     $('.comment').html('That is the incorrect answer');
+    //     $('.scoreboard').html(score -= 100)
+    //     $(event.target).off()
+    // })
+
+    // $('#button-C').on('click', () => {
+    //     $('.comment').html('That is the incorrect answer');
+    //     $('.scoreboard').html(score -= 100)
+    //     $(event.target).off()
+    // });
+
+
+// displays question
+$('.his-200').on('click', () => {
+    $('.questions').html('Which president, the first to receive a Secret Service code name, was called "General," even though he was only a captain in World War I?');
+    $('.answerA').html('Abrham Lincoln');
+    $('.answerB').html('Harry Truman');
+    $('.answerC').html('Theodore Roosevelt');
+    $(event.target).off();
+    $(event.target).css( "color", "red");
+    answers = "B"
+    // correct or incorrect answers
+    $('#button-B').on('click', () => {
+        playerAnswer = "B"
+        if(playerAnswer === answers){
+            $('.scoreboard').html(score += 100)
+            $('.comment').html('That is the correct answer');        
+        } else {
+            $('.scoreboard').html(score -= 100)
+            $('.comment').html('That is the incorrect answer');       
+        }     
+        $(event.target).off()
+        })
+
+    $('#button-A').on('click', () => {
+        playerAnswer = "A"
+        if(playerAnswer === answers){
+            $('.scoreboard').html(score += 100)
+            $('.comment').html('That is the correct answer');        
+        } else {
+            $('.scoreboard').html(score -= 100)
+            $('.comment').html('That is the incorrect answer');       
+        }     
+        $(event.target).off()
+    })
+
+    $('#button-C').on('click', () => {
+        playerAnswer = "C"
+        if(playerAnswer === answers){
+            $('.scoreboard').html(score += 100)
+            $('.comment').html('That is the correct answer');        
+        } else {
+            $('.scoreboard').html(score -= 100)
+            $('.comment').html('That is the incorrect answer');       
+        }     
+        $(event.target).off()
+    });
+})
+
+
+// correct or incorrect answers
+//     $('#button-B').on('click', () => {
+//         $('.comment').html('That is the correct answer');
+//         $('.scoreboard').html(score += 100)
+//         $(event.target).off()
+//         })
+
+//     $('#button-A').on('click', () => {
+//         $('.comment').html('That is the incorrect answer');
+//         $('.scoreboard').html(score -= 100)
+//         $(event.target).off()
+//     })
+
+//     $('#button-C').on('click', () => {
+//         $('.comment').html('That is the incorrect answer');
+//         $('.scoreboard').html(score -= 100)
+//         $(event.target).off()
+//     });
+    
