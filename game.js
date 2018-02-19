5ee2fc077134e3ab0c550e8c9beb004f442eecfe
FarrukhKhalikoff <farrukhkhalikov@yahoo.com>
2018-02-18 21:51:33 -0500

    added buttons

---
+++ game.js
@@ -0,0 +1,20 @@
+//Scoreboard
+    //start with 0
+        //Allow for score to be changed based on correct answer and amount (+ or -)
+
+
+
+
+
+//Answerboard
+    //Button is clicked
+
+        //Place question in div and different answers in answerboard
+
+
+//Click for answer
+if (button A) {
+    add 100 to score
+} else {
+    debit 100 from score
+}
