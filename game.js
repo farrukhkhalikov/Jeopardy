5c277ff724e69bbff9db6c2f7ad182d336420cff
FarrukhKhalikoff <farrukhkhalikov@yahoo.com>
2018-02-18 22:09:14 -0500

    question pop up logic

--- game.js
+++ game.js
@@ -2,9 +2,12 @@
     //start with 0
         //Allow for score to be changed based on correct answer and amount (+ or -)
 
+let score = 0;
+$('.scoreboard').html(score)
 
-
-
+$('.his-100').on('click', () => {
+    $('.questions').html('Christopher Columbus sailed the ocean blue on this year')
+})
 
 //Answerboard
     //Button is clicked
@@ -13,8 +16,8 @@
 
 
 //Click for answer
-if (button A) {
-    add 100 to score
-} else {
-    debit 100 from score
-}
+// if (button A) {
+//     add 100 to score
+// } else {
+//     debit 100 from score
+// }
