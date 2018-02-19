98c505be7e4c1b9a352050bdae11e239ec8d7b05
FarrukhKhalikoff <farrukhkhalikov@yahoo.com>
2018-02-19 12:10:28 -0500

    refactored more code

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ let score = 0;
 let answers = ''
 let playerAnswer = ""
 $('.scoreboard').html(score)
+
 function checkForAnswer(points){
     if(playerAnswer === answers){
         $('.scoreboard').html(score += points)
@@ -10,9 +11,34 @@ function checkForAnswer(points){
     } else {
         $('.scoreboard').html(score -= points)
         $('.comment').html('That is the incorrect answer');       
-    } 
-};
+    }
+} 
+function answerButtons(points){
+    $('#button-A').on('click', () => {
+        playerAnswer = "A"
+        checkForAnswer(points)
+        $('#button-B').off()
+        $('#button-C').off()
+        $(event.target).off()
+        })
 
+    $('#button-B').on('click', () => {
+        playerAnswer = "B"
+       checkForAnswer(points)
+        $('#button-A').off()
+        $('#button-C').off()
+        $(event.target).off()
+    })
+
+    $('#button-C').on('click', () => {
+        playerAnswer = "C"
+        checkForAnswer(points)
+        $('#button-A').off()
+        $('#button-B').off()
+        $(event.target).off()
+    });
+
+}
 // displays question
 $('.his-100').one('click', () => {
     $('.questions').html('Ulysses S. Grant appears on the front of which denomination of US. currency');
@@ -23,29 +49,7 @@ $('.his-100').one('click', () => {
     $(event.target).css( "color", "red");
     answers = "A"
     // correct or incorrect answers
-    $('#button-A').on('click', () => {
-        playerAnswer = "A"
-        checkForAnswer(100)
-        $('#button-B').off()
-        $('#button-C').off()
-        $(event.target).off()
-        })
-
-    $('#button-B').on('click', () => {
-        playerAnswer = "B"
-       checkForAnswer(100)
-        $('#button-A').off()
-        $('#button-C').off()
-        $(event.target).off()
-    })
-
-    $('#button-C').on('click', () => {
-        playerAnswer = "C"
-        checkForAnswer(100)
-        $('#button-A').off()
-        $('#button-B').off()
-        $(event.target).off()
-    });
+    answerButtons(100);
     
 })
 
@@ -58,28 +62,6 @@ $('.his-200').one('click', () => {
     $(event.target).css( "color", "red");
     answers = "B"
     // correct or incorrect answers
-    $('#button-B').on('click', () => {
-        playerAnswer = "B"
-        checkForAnswer(200)
-        $('#button-A').off()
-        $('#button-C').off()
-        $(event.target).off()
-        })
-
-    $('#button-A').on('click', () => {
-        playerAnswer = "A"
-        checkForAnswer(200)
-        $('#button-B').off()
-        $('#button-C').off()
-        $(event.target).off()
-    })
-
-    $('#button-C').on('click', () => {
-        playerAnswer = "C"
-        checkForAnswer(200)
-        $('#button-A').off()
-        $('#button-B').off()
-        $(event.target).off()
-    });
+    answerButtons(200)
 })
 
